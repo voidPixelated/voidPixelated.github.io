f77e86a79060781ef864a84ae8d4e3556053e291
Void <voidpixelated@gmail.com>
2025-12-02 19:45:40 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1445008211722768488/CKYed9HMxYVUmIm20duoxe6r6Nld5uvmJ6NXXTIw0AX01CULNZPETKp5oz0Znttd6cfv'
-var regions = 'IP_DEFAULT_REGION'; // Dont Delete/Change Here !
+
